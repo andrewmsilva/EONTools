c2e7115b231b88043f41ad1ff86de5bb1badb04e
Andrew M. Silva <andrewsaxx@gmail.com>
2019-10-02 10:25:01 -0300

    Fixing a mistake

--- Test.py
+++ Test.py
@@ -20,7 +20,6 @@ simulation_options = {
 
 print('\nOriginal graph simulation')
 eon.random_simulation(**simulation_options)
-print(eon.demands[0])
 print(eon.reports_from_demands())
 
 possible_eons = et.get_all_possible_eons_with_new_links_by_length(eon, 50, 1, n_links=1, max_length=reports['diameter_by_length'] / 2)
