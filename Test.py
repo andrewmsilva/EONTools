c813ffe40d389dc967df84d3023602b5b6d3c42b
Andrew M. Silva <andrewsaxx@gmail.com>
2019-10-02 10:18:35 -0300

    Changing structure of spectrum, demands, simulations, reports and comments

--- Test.py
+++ Test.py
@@ -3,10 +3,10 @@ import EONTools as et
 nodes_csv = 'networks/rnp/rnpBrazil_nodes.csv'
 links_csv = 'networks/rnp/rnpBrazil_links.csv'
 
-eon = et.EON()
+eon = et.EON(results_folder='results/')
 eon.load_csv(nodes_csv, links_csv)
 
-reports = eon.reports()
+reports = eon.full_reports()
 eon.print_reports(reports)
 eon.save_reports(reports)
 eon.save_figure()
@@ -19,11 +19,11 @@ simulation_options = {
 }
 
 print('\nOriginal graph simulation')
-demands = et.random_simulation(eon, **simulation_options)
-print(et.reports_from_demands(demands))
+eon.random_simulation(eon, **simulation_options)
+print(eon.reports_from_demands())
 
 possible_eons = et.get_all_possible_eons_with_new_links_by_length(eon, 50, 1, n_links=1, max_length=reports['diameter_by_length'] / 2)
 for i in range(len(possible_eons)):
     print('\nGraph %d simulation'%i)
-    demands = et.random_simulation(possible_eons[i], **simulation_options)
-    print(et.reports_from_demands(demands))
+    eon.random_simulation(possible_eons[i], **simulation_options)
+    print(eon.reports_from_demands())
